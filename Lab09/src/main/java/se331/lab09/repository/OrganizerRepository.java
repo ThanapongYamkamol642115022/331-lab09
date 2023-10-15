529d2b02e80e0e844fa779bdac19a33bed69ceea
thanap151255 <thanap151255@gmail.com>
2023-10-15 15:25:00 +0700

    2.2 - 7

--- Lab09/src/main/java/se331/lab09/repository/OrganizerRepository.java
+++
@@ -1,7 +0,0 @@
-package se331.lab09.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se331.lab08_1.entity.Organizer;
-
-public interface OrganizerRepository extends JpaRepository<Organizer,Long> {
-}
